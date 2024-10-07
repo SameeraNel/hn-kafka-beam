464925a4eb8aa247309dbe136bfb52a51dcaa1dc
Sameera Nelson <sameira@gmail.com>
2024-10-07 21:50:00 +0530

    Add files via upload

---
+++ agepublisher/src/main/java/com/demo/agepublisher/AgepublisherApplication.java
@@ -0,0 +1,54 @@
+package com.demo.agepublisher;
+
+import com.demo.agepublisher.enums.KafkaTopics;
+import com.demo.agepublisher.functions.AgeProcessingFunction;
+import com.demo.agepublisher.functions.MessagePublishFunction;
+import org.apache.beam.runners.direct.DirectRunner;
+import org.apache.beam.sdk.io.kafka.KafkaIO;
+import org.apache.beam.sdk.options.*;
+import org.apache.beam.sdk.transforms.ParDo;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.apache.kafka.common.serialization.StringDeserializer;
+
+import org.apache.beam.sdk.Pipeline;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+
+@SpringBootApplication
+public class AgepublisherApplication implements ApplicationListener<ContextRefreshedEvent> {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgepublisherApplication.class, args);
+	}
+
+	@Override
+	public void onApplicationEvent(ContextRefreshedEvent event) {
+		runBeamPipelineLine();
+	}
+
+	private void runBeamPipelineLine() {
+		PipelineOptions options = PipelineOptionsFactory.create();
+		options.setRunner(DirectRunner.class);
+		Pipeline pipeline = Pipeline.create(options);
+
+		String kafkaBootstrapServers = "localhost:9092";
+		String inputTopic = KafkaTopics.CUSTOMER_INPUT.getTopicName();
+		String evenTopic = KafkaTopics.CUSTOMER_EVEN.getTopicName();
+		String oddTopic = KafkaTopics.CUSTOMER_ODD.getTopicName();
+
+		pipeline
+				.apply("ReadMessagesFromKafka",
+						KafkaIO.<String, String>read()
+								.withBootstrapServers(kafkaBootstrapServers)
+								.withTopic(inputTopic)
+								.withKeyDeserializer(StringDeserializer.class)
+								.withValueDeserializer(StringDeserializer.class)
+								.withoutMetadata())
+				.apply("ProcessMessages", ParDo.of(new AgeProcessingFunction()))
+				.apply("PublishMessages", ParDo.of(new MessagePublishFunction(kafkaBootstrapServers, evenTopic, oddTopic)));
+
+		pipeline.run().waitUntilFinish();
+	}
+
+}
